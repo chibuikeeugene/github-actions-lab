7926b2d34b922bf3031afee939fa8bb15b5ba4fe
chibuikeeugene <chibuikeeugene@gmail.com>
2026-07-27 13:55:47 +0200

    added quality check to ci yml file and updated requirements txt file- updated

--- calculator.py
+++ calculator.py
@@ -1,16 +1,19 @@
 # a basic calc
-def add(num1:float, num2:float) -> float:
+def add(num1: float, num2: float) -> float:
     """add two numbers"""
     return num1 + num2
 
-def subtract(num1:float, num2:float) -> float:
+
+def subtract(num1: float, num2: float) -> float:
     """substract two numbers"""
     return num1 - num2
 
-def multiply(num1:float, num2:float) -> float:
+
+def multiply(num1: float, num2: float) -> float:
     """multiply two numbers"""
     return num1 * num2
 
-def divide(num1:float, num2:float) -> float:
+
+def divide(num1: float, num2: float) -> float:
     """divide two numbers"""
     return num1 / num2
--- test_calculator.py
+++ test_calculator.py
@@ -1,18 +1,22 @@
 from calculator import add, divide, multiply, subtract
 
+
 def test_add() -> None:
     """run test on add operation"""
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
+
 
 def test_subtract() -> None:
     """run test on substract operation"""
-    assert subtract(2,3) == -1
+    assert subtract(2, 3) == -1
+
 
 def test_multiply() -> None:
     """run test on multiply operation"""
-    assert multiply(4,5) == 20
+    assert multiply(4, 5) == 20
     assert multiply(-4, 5) == -20
+
 
 def test_divide() -> None:
     """run test on division operation"""
-    assert divide(4,2) == 2
+    assert divide(4, 2) == 2
