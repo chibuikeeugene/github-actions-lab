3052667c77951c6a15b354463220e7a78ebbb959
chibuikeeugene <chibuikeeugene@gmail.com>
2026-07-31 11:29:59 +0200

    code restructuring reversal

--- test_calculator.py
+++ test_calculator.py
@@ -1,18 +1,29 @@
-from calculator import add, subtract, multiply, divide
+import sys
+
+from calculator import add, divide, multiply, subtract
+
 
 def test_add() -> None:
     """run test on add operation"""
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
+
 
 def test_subtract() -> None:
     """run test on substract operation"""
-    assert subtract(2,3) == -1
+    assert subtract(2, 3) == -1
+
 
 def test_multiply() -> None:
     """run test on multiply operation"""
-    assert multiply(4,5) == 20
+    assert multiply(4, 5) == 20
     assert multiply(-4, 5) == -20
+
 
 def test_divide() -> None:
     """run test on division operation"""
-    assert divide(4,2) == 2
+    assert divide(4, 2) == 2
+
+
+def test_minimum_python_version() -> None:
+    """run test to check python version"""
+    assert sys.version_info >= (3, 11)
